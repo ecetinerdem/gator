d650b7e121e47d69315fc4e4f3436133913eb51e
Cetin Erdem <cetoerdem1@gmail.com>
2025-10-21 01:57:22 +0400

    Add files via upload

---
+++ sql/queries/feeds.sql
@@ -0,0 +1,36 @@
+-- name: CreateFeed :one
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6
+)
+RETURNING *;
+
+-- name: GetFeed :one
+SELECT * FROM feeds
+WHERE name = $1;
+
+-- name: GetFeedByURL :one
+SELECT * FROM feeds
+WHERE url = $1;
+
+-- name: DeleteFeeds :exec
+DELETE FROM feeds;
+
+-- name: GetFeeds :many
+SELECT * FROM feeds;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(),
+    updated_at = NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
